e3933c566c75571180ac5badb927d59e8c2ea687
Brett Kinnamon <b.kinnamon@icloud.com>
2019-10-01 12:56:12 -0400

    fixes #1 make content for the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,42 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Brett Kinnamon</h1>
-    <small>Web developer</small>
-  </Layout>
-)
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query PostsQuery {
+      allMarkdownRemark(filter: {frontmatter: {category: {eq: "posts"}}}) {
+        edges {
+          node {
+            id
+            frontmatter {
+              title
+            }
+            excerpt
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <section className="my-6">
+        <h1>Brett Kinnamon</h1>
+        <p className="subtitle">Web developer</p>
+      </section>
+
+      {data.allMarkdownRemark.edges.map(({node}) => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>{node.excerpt}</p>
+        </article>
+      ))}
+    </Layout>
+  )
+}
 
 export default IndexPage
