6b62e406729b65c9df3c3c2fe3cca9ba2b01e3b8
Brett Kinnamon <b.kinnamon@icloud.com>
2019-10-31 07:47:22 -0400

    add syntax highlighting  to blog posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,15 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [{
+          resolve: `gatsby-remark-vscode`,
+          options: {}
+        }]
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
